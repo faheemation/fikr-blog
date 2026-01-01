a47044a029d5716661f52f9f3652ec4bc36f22b0
Faheem <mehfilvaliyannur@gmail.com>
2026-01-02 00:34:07 +0530

    Increase Fikr logo size

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -67,9 +67,9 @@ export default function Navigation() {
                         <Image
                             src="/fikr-logo.png"
                             alt="Fikr"
-                            width={120}
-                            height={40}
-                            className="h-10 w-auto"
+                            width={180}
+                            height={60}
+                            className="h-12 w-auto"
                             priority
                         />
                     </Link>
