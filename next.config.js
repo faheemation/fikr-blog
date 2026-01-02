80f9d25f4a85b651c7c2ddda5d301402ebba290f
Faheem <mehfilvaliyannur@gmail.com>
2026-01-02 11:20:16 +0530

    Fix: Add unoptimized flag to fix Cloudinary image display

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true, // Disable Next.js image optimization to fix Cloudinary image display
         remotePatterns: [
             {
                 protocol: 'https',
